0198f700204834d3a7e0a9c8bab7c749dc1552c5
Kristhinne <krs.opie@gmail.com>
2024-11-29 09:56:36 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/modulo-3-evaluacion-final-krsalvarez/",
+  base: "modulo-3-evaluacion-final-krsalvarez",
 })
